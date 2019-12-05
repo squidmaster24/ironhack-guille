869674f6d21ce5b38fb000724f993b07a1ae309e
Guillermo Escanciano <guillermoglc@gmail.com>
2019-12-05 09:48:10 +0100

    Exercise 01 finished

--- js/index.js
+++ js/index.js
@@ -69,7 +69,7 @@ function lexicalOrder(word1, word2) {
 //BONUS
 
 let lorem = String;
-let lookingForWord = "et";
+let lookingForWord = 'et';
 lorem =
   "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed quis justo nec nunc egestas consequat. Ut suscipit eu nisi et imperdiet. Quisque vel mi ligula. Interdum et malesuada fames ac ante ipsum primis in faucibus. Maecenas sit amet arcu vitae lacus pretium feugiat. Sed auctor viverra tortor, eget maximus orci. Fusce pellentesque suscipit sapien. Etiam tincidunt diam sed leo scelerisque, vel auctor eros lobortis. Nullam in dapibus eros. Vivamus placerat quis tortor quis consequat. Vivamus id augue ante. Phasellus nec nulla quam. In fermentum quam at lorem ultrices, in fringilla erat rhoncus. Cras eget magna rhoncus, commodo orci id, placerat sem. Ut elementum dapibus nunc, eu dapibus neque vestibulum a. Etiam tincidunt libero mi, eget vestibulum nunc facilisis in. Proin eu malesuada ligula. Aenean tempus velit at nibh commodo, vel vestibulum massa sollicitudin. Sed ut aliquam tortor. Vivamus vulputate sollicitudin dignissim. Suspendisse dolor elit, tincidunt eget blandit eget, vehicula ut libero. Vivamus ultrices dolor mi. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Proin pharetra leo non pretium elementum. Integer ac ante massa. In ac ullamcorper tortor, a consequat lectus. Nam nec sollicitudin tellus. Nulla facilisi. Etiam dolor ex, tristique vitae volutpat ut, elementum nec ante. Morbi at orci sed mauris vulputate consectetur ut quis arcu. Donec nec consectetur leo, non ornare libero. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse bibendum nibh sed elementum malesuada. Nulla aliquet tellus in tincidunt rhoncus. Nam a odio tincidunt, ullamcorper purus eget, facilisis mi. Proin ac ultricies risus. Suspendisse potenti. Sed suscipit pharetra mauris in tincidunt.";
 
@@ -77,59 +77,54 @@ countWords(lorem);
 // findWord(lorem, lookingForWord);
 findWordFor(lorem, lookingForWord);
 
-chooseWordToFind();
-
 function countWords(textString) {
   let totalWords = Number;
   totalWords = textString.trim().split(/\s+/).length;
   return console.log("The string has:", totalWords, "words.");
 }
 
-function findWord(textString, wordToFind) {
-  let timesRepeated = Number;
-  timesRepeated = textString.trim().split(wordToFind).length;
-  return console.log(
-    "The word",
-    wordToFind,
-    "appears",
-    timesRepeated,
-    "times."
-  );
-}
-
-// function findWordFor(text, word) {
-//   let count = 0;
-//   for (let i = 0; i < text.length; i++) {
-//     if (text[i] === " ") {
-//       if (
-//         text[i + 1] === 'e' &&
-//         text[i + 2] === 't' &&
-//         text[i + 3] === " "
-//       ) {
-//         count++;
-//       }
-//     }
-//   }
-//   console.log("La palabra", word, "aparece", count, "veces.");
-//   console.log(word[0]);
-//   console.log(word[1]);
+// function findWord(textString, wordToFind) {
+//   let timesRepeated = Number;
+//   timesRepeated = textString.trim().split(wordToFind).length;
+//   return console.log(
+//     "The word",
+//     wordToFind,
+//     "appears",
+//     timesRepeated,
+//     "times."
+//   );
 // }
 
 function findWordFor(text, word) {
   let count = 0;
-  let j = 0;
   for (let i = 0; i < text.length; i++) {
     if (text[i] === " ") {
-      while (text[i])
-        for (let j = 0; (i = word.length); j++) {
-          text[i] === word[j];
-        }
-
-      if (text[i + 1] === "e" && text[i + 2] === "t" && text[i + 3] === " ") {
+      if (
+        text[i + 1] === word[0] &&
+        text[i + 2] === word[1] &&
+        text[i + 3] === " "
+      ) {
         count++;
       }
     }
   }
-  console.log("La palabra", word, "aparece", count, "veces.")
+  console.log("La palabra", word, "aparece", count, "veces.");
 }
 
+// function findWordFor(text, word) {
+//   let count = 0;
+//   let j = 0;
+//   for (let i = 0; i < text.length; i++) {
+//     if (text[i] === " ") {
+//       while (text[i])
+//         for (let j = 0; (i = word.length); j++) {
+//           text[i] === word[j];
+//         }
+
+//       if (text[i + 1] === "e" && text[i + 2] === "t" && text[i + 3] === " ") {
+//         count++;
+//       }
+//     }
+//   }
+//   console.log("La palabra", word, "aparece", count, "veces.")
+// }
